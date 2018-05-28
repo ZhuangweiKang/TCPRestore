9a14a229665781f1fb6c72a51e9260d4ef24e31d
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-05-27 20:22:01 -0500

    update

--- dependences.sh
+++ dependences.sh
@@ -2,7 +2,8 @@
 
 # check if docker exists
 docker_check=$(which docker)
-if [ "$docker_check"="" ]
+
+if [ "$docker_check"="" ];
 then
     # install docker
     cd /home/ubuntu
@@ -12,14 +13,13 @@ then
     # run docker without sudo
     groupadd docker
     usermod -aG docker $USER
-
 fi
 
 
 # check docker experimental feature
 docker_version=$(docker version | grep Experimental)
 
-if [ "$docker_version"="$false" ]
+if [ "$docker_version"="$false" ];
 then
     echo "{\"experimental\": true}" >> /etc/docker/daemon.json
     # restart docker daemon
@@ -31,7 +31,7 @@ echo $(docker version)
 
 # check if CRIU exists
 criu_check=$(which criu)
-if [ "$criu_check"="" ]
+if [ "$criu_check"="" ];
 then
     apt-get update && sudo apt-get install -y protobuf-c-compiler libprotobuf-c0-dev protobuf-compiler libprotobuf-dev:amd64 gcc build-essential bsdmainutils python git-core asciidoc make htop git curl supervisor cgroup-lite libapparmor-dev libseccomp-dev libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler python-protobuf libnl-3-dev libcap-dev libaio-dev apparmor libnet-dev
     cd /home/ubuntu
